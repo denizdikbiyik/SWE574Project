1cf6a6b57681792d739c7f839f902d32707a8085
Mac <mac@mac-macbook-pro.mynet>
2021-12-04 04:30:02 +0300

    is_active for UI, admin application view

--- social/admin.py
+++ social/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Service, Event, UserProfile
-#from .models import Service, Event, Feedback, UserProfile
+from .models import Service, Event, UserProfile, ServiceApplication
+#from .models import Service, Event, Feedback, UserProfile, ServiceApplication
 
 admin.site.register(Service)
 admin.site.register(Event)
 #admin.site.register(Feedback)
 admin.site.register(UserProfile)
+admin.site.register(ServiceApplication)
