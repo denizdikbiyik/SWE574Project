b5179fd1e5457e913add9d9fcea64a215b89f46e
Mac <mac@mac-macbook-pro.mynet>
2021-12-31 19:39:18 +0300

    testsupdated

--- social/tests/test_models.py
+++ social/tests/test_models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from django.test import TestCase
 
 from django.contrib.auth.models import User
-from social.models import Service, Feedback, UserProfile, Event, ServiceApplication
+from social.models import Service, UserProfile, Event, ServiceApplication, UserRatings, NotifyUser, EventApplication, Tag
 
 class ServiceModelTest(TestCase):              
     @classmethod
@@ -15,8 +15,9 @@ class ServiceModelTest(TestCase):
             creater=self.u1, 
             createddate=datetime.now, 
             name="ServiceTest",
+            description="ServiceTestDescription", 
             picture='uploads/service_pictures/default.png',
-            description="ServiceTestDescription",   
+            location='41.0255493,28.9742571',
             servicedate=datetime.now,
             capacity=1,
             duration=1,
@@ -26,6 +27,22 @@ class ServiceModelTest(TestCase):
         self.assertEqual(service.creater, self.u1)
         self.assertEqual(service.createddate, datetime.now)
         self.assertEqual(service.name, "ServiceTest")
+
+    def testEvent(self):
+        event = Event(
+            eventcreater=self.u1, 
+            eventcreateddate=datetime.now, 
+            eventname="EventTest",
+            eventdescription="EventTestDescription", 
+            eventpicture='uploads/event_pictures/default.png',
+            eventlocation='41.0255493,28.9742571',
+            eventdate=datetime.now,
+            eventcapacity=1,
+            eventduration=1
+            )
+        self.assertEqual(event.eventcreater, self.u1)
+        self.assertEqual(event.eventcreateddate, datetime.now)
+        self.assertEqual(event.eventname, "EventTest")
 
     def tearDown(self):
         self.u1.delete()
--- social/tests/test_urls.py
+++ social/tests/test_urls.py
@@ -1,14 +1,157 @@
 from django.test import TestCase
-#from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from social.views import AllServicesView, ServiceDetailView, ServiceEditView, ServiceDeleteView
+from social.views import ServiceCreateView, ServiceDetailView, ServiceEditView, ServiceDeleteView, EventCreateView, EventDetailView, EventEditView, EventDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, ApplicationDeleteView, ApplicationEditView, FollowersListView, RemoveMyFollower, TimeLine, AllServicesView, AllEventsView, CreatedServicesView, CreatedEventsView, AppliedServicesView, ConfirmServiceTaken, ConfirmServiceGiven, RateUser, RateUserDelete, RateUserEdit, ServiceSearch, EventSearch, Notifications, EventApplicationDeleteView, AppliedEventsView, RequestCreateView, CreatedRequestsView, RequestsFromMeView, RequestDetailView, RequestDeleteView, ServiceFilter
 
 class URLTests(TestCase):
-#class TestUrls(SimpleTestCase):
+
+    def test_service_create_url_resolves(self):
+        url = reverse('service-create')
+        self.assertEquals(resolve(url).func.view_class, ServiceCreateView)
 
     def test_service_list_url_resolves(self):
-        #assert 1 == 2
         url = reverse('allservices')
-        #print(resolve(url))
         self.assertEquals(resolve(url).func.view_class, AllServicesView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('createdservices')
+        self.assertEquals(resolve(url).func.view_class, CreatedServicesView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('appliedservices')
+        self.assertEquals(resolve(url).func.view_class, AppliedServicesView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('service-detail')
+        self.assertEquals(resolve(url).func.view_class, ServiceDetailView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('service-edit')
+        self.assertEquals(resolve(url).func.view_class, ServiceEditView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('service-delete')
+        self.assertEquals(resolve(url).func.view_class, ServiceDeleteView)
     
+    def test_service_list_url_resolves(self):
+        url = reverse('profile')
+        self.assertEquals(resolve(url).func.view_class, ProfileView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('profile-edit')
+        self.assertEquals(resolve(url).func.view_class, ProfileEditView)
+    
+    def test_service_list_url_resolves(self):
+        url = reverse('event-create')
+        self.assertEquals(resolve(url).func.view_class, EventCreateView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('allevents')
+        self.assertEquals(resolve(url).func.view_class, AllEventsView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('createdevents')
+        self.assertEquals(resolve(url).func.view_class, CreatedEventsView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('event-detail')
+        self.assertEquals(resolve(url).func.view_class, EventDetailView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('event-edit')
+        self.assertEquals(resolve(url).func.view_class, EventEditView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('event-delete')
+        self.assertEquals(resolve(url).func.view_class, EventDeleteView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('add-follower')
+        self.assertEquals(resolve(url).func.view_class, AddFollower)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('remove-follower')
+        self.assertEquals(resolve(url).func.view_class, RemoveFollower)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('remove-my-follower')
+        self.assertEquals(resolve(url).func.view_class, RemoveMyFollower)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('application-delete')
+        self.assertEquals(resolve(url).func.view_class, ApplicationDeleteView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('application-edit')
+        self.assertEquals(resolve(url).func.view_class, ApplicationEditView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('confirm-service-taken')
+        self.assertEquals(resolve(url).func.view_class, ConfirmServiceTaken)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('confirm-service-given')
+        self.assertEquals(resolve(url).func.view_class, ConfirmServiceGiven)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('followers')
+        self.assertEquals(resolve(url).func.view_class, FollowersListView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('rateuser')
+        self.assertEquals(resolve(url).func.view_class, RateUser)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('rating-edit')
+        self.assertEquals(resolve(url).func.view_class, RateUserEdit)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('rating-delete')
+        self.assertEquals(resolve(url).func.view_class, RateUserDelete)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('timeline')
+        self.assertEquals(resolve(url).func.view_class, TimeLine)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('service-search')
+        self.assertEquals(resolve(url).func.view_class, ServiceSearch)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('event-search')
+        self.assertEquals(resolve(url).func.view_class, EventSearch)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('notifications')
+        self.assertEquals(resolve(url).func.view_class, Notifications)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('event-application-delete')
+        self.assertEquals(resolve(url).func.view_class, EventApplicationDeleteView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('appliedevents')
+        self.assertEquals(resolve(url).func.view_class, AppliedEventsView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('request-create')
+        self.assertEquals(resolve(url).func.view_class, RequestCreateView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('createdrequests')
+        self.assertEquals(resolve(url).func.view_class, CreatedRequestsView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('requestsfromme')
+        self.assertEquals(resolve(url).func.view_class, RequestsFromMeView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('request-detail')
+        self.assertEquals(resolve(url).func.view_class, RequestDetailView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('request-delete')
+        self.assertEquals(resolve(url).func.view_class, RequestDeleteView)
+
+    def test_service_list_url_resolves(self):
+        url = reverse('service-filter')
+        self.assertEquals(resolve(url).func.view_class, ServiceFilter)
