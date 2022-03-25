8b6bdb919298a3baf83746a4c4cb7a51be44d283
Deniz Dikbıyık <denizdikbiyik@gmail.com>
2022-03-25 18:26:12 +0300

    Add New Google API Key for Location #14

--- CommUnitySocial/settings.py
+++ CommUnitySocial/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
 
 LOCATION_FIELD = {
     'provider.google.api': '//maps.google.com/maps/api/js?sensor=false',
-    'provider.google.api_key': 'AIzaSyCCMF-2zpHGf_H_5mpYCmSShHkCwmZbZXY',
+    'provider.google.api_key': 'AIzaSyAAsiQaM2KE_UZ_k7gSQZfIGtF0lAiBCBQ',
     'provider.google.api_libraries': '',
     'provider.google.map.type': 'ROADMAP',
 }
