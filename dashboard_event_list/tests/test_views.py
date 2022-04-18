b0e198ad3d0716ac420bf9462a7dc06cb9471c50
Alper Turgal <alper.turgal@gmail.com>
2022-04-18 22:53:25 +0300

    Add Pagination #67
    [BUG] #85 #89 #90 #91
    
    correct commit

--- dashboard_event_list/tests/test_views.py
+++ dashboard_event_list/tests/test_views.py
@@ -4,33 +4,20 @@ from django.http import HttpRequest
 from django.test import TestCase, Client
 from django.urls import reverse
 import datetime
-from dashboard_event_list.views import make_context_for_event_list
+from dashboard_event_list.views import make_query_for_event_list
 from social.models import Event
 from django.contrib.auth.models import User
 
 
 class TestViews(TestCase):
-    '''
-    # P
-    def test_url_accessible_by_name(self):
-        client = Client()
-        response = client.get(reverse("eventlist"))
-        self.assertEquals(response.status_code, 404)
-
-    # P
-    def test_view_uses_correct_template(self):
-        client = Client()
-        response = client.get(reverse("eventlist"))
-        self.assertTemplateUsed(response, "dashboard_event_list/eventlist.html")
 
     # P
     def test_url_exists(self):
         client = Client()
-        response = client.get("/dashboardevent/eventlist/")
+        response = client.get("/dashboard/eventlist/")
         self.assertEqual(response.status_code, 404)
-    '''
 
-    def test_make_context_for_service_list(self):
+    def test_make_query_for_service_list(self):
         test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
         test_user1.profile.isAdmin = True
         test_user1.save()
@@ -89,40 +76,55 @@ class TestViews(TestCase):
 
         # test period selection "all"
         field1 = "all"
-        field2 = None
-        field3 = None
-        field4 = "createddate"
-        context = make_context_for_event_list(field1, field2, field3, field4)
-        event_count = context["events"].count()
-        print(context["events"].count())
-        self.assertEquals(event_count, 3)
+        field2 = create_date_before_given_date(501, date_today)
+        field3 = date_today
+        period = make_query_for_event_list(field1, field2, field3)
+        services = Event.objects.filter(eventcreateddate__gte=period["date_old"],
+                                        eventcreateddate__lte=period["date_new"])
+        service_count = services.count()
+        print(service_count)
+        self.assertEquals(service_count, 3)
 
         # test period selection "week"
         field1 = "week"
-        field2 = None
-        field3 = None
-        field4 = "createddate"
-        context = make_context_for_event_list(field1, field2, field3, field4)
-        event_count = context["events"].count()
-        print(context["events"].count())
-        self.assertEquals(event_count, 1)
+        field2 = create_date_before_given_date(501, date_today)
+        field3 = date_today
+        period = make_query_for_event_list(field1, field2, field3)
+        services = Event.objects.filter(eventcreateddate__gte=period["date_old"],
+                                        eventcreateddate__lte=period["date_new"])
+        service_count = services.count()
+        print(service_count)
+        self.assertEquals(service_count, 1)
 
         # test period selection "month"
         field1 = "month"
-        field2 = None
-        field3 = None
-        field4 = "createddate"
-        context = make_context_for_event_list(field1, field2, field3, field4)
-        event_count = context["events"].count()
-        print(context["events"].count())
-        self.assertEquals(event_count, 2)
-
-        # test period selection ""select""
-        field1 = "select"
         field2 = create_date_before_given_date(501, date_today)
         field3 = date_today
-        field4 = "createddate"
-        context = make_context_for_event_list(field1, field2, field3, field4)
-        event_count = context["events"].count()
-        print(context["events"].count())
-        self.assertEquals(event_count, 3)
+        period = make_query_for_event_list(field1, field2, field3)
+        services = Event.objects.filter(eventcreateddate__gte=period["date_old"],
+                                        eventcreateddate__lte=period["date_new"])
+        service_count = services.count()
+        print(service_count)
+        self.assertEquals(service_count, 2)
+
+        # test period selection "year"
+        field1 = "year"
+        field2 = create_date_before_given_date(501, date_today)
+        field3 = date_today
+        period = make_query_for_event_list(field1, field2, field3)
+        services = Event.objects.filter(eventcreateddate__gte=period["date_old"],
+                                        eventcreateddate__lte=period["date_new"])
+        service_count = services.count()
+        print(service_count)
+        self.assertEquals(service_count, 2)
+
+        # test period selection ""select""
+        field1 = ""
+        field2 = create_date_before_given_date(501, date_today)
+        field3 = date_today
+        period = make_query_for_event_list(field1, field2, field3)
+        services = Event.objects.filter(eventcreateddate__gte=period["date_old"],
+                                        eventcreateddate__lte=period["date_new"])
+        service_count = services.count()
+        print(service_count)
+        self.assertEquals(service_count, 3)
