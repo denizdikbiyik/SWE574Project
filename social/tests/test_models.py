74f2096c9dcfd6065d6e31df06fff9e5801d6ff5
Alper Turgal <alper.turgal@gmail.com>
2022-04-22 00:50:39 +0300

    Retrieve Wikidata Description for Services Created #133, test correction.

--- social/tests/test_models.py
+++ social/tests/test_models.py
@@ -2,20 +2,22 @@ from datetime import datetime
 from django.test import TestCase
 
 from django.contrib.auth.models import User
-from social.models import Service, UserProfile, Event, ServiceApplication, UserRatings, NotifyUser, EventApplication, Tag
+from social.models import Service, UserProfile, Event, ServiceApplication, UserRatings, NotifyUser, EventApplication, \
+    Tag
 
-class ServiceEventModelTest(TestCase):              
+
+class ServiceEventModelTest(TestCase):
     @classmethod
-
     def setUp(self):
         self.u1 = User.objects.create(username='usertest1')
 
     def testService(self):
         service = Service(
-            creater=self.u1, 
-            createddate=datetime.now, 
+            creater=self.u1,
+            createddate=datetime.now,
             name="ServiceTest",
-            description="ServiceTestDescription", 
+            description="ServiceTestDescription",
+            wiki_description="ServiceTestWikiDescription",
             picture='uploads/service_pictures/default.png',
             location='41.0255493,28.9742571',
             servicedate=datetime.now,
@@ -23,23 +25,23 @@ class ServiceEventModelTest(TestCase):
             duration=1,
             is_given=False,
             is_taken=False
-            )
+        )
         self.assertEqual(service.creater, self.u1)
         self.assertEqual(service.createddate, datetime.now)
         self.assertEqual(service.name, "ServiceTest")
 
     def testEvent(self):
         event = Event(
-            eventcreater=self.u1, 
-            eventcreateddate=datetime.now, 
+            eventcreater=self.u1,
+            eventcreateddate=datetime.now,
             eventname="EventTest",
-            eventdescription="EventTestDescription", 
+            eventdescription="EventTestDescription",
             eventpicture='uploads/event_pictures/default.png',
             eventlocation='41.0255493,28.9742571',
             eventdate=datetime.now,
             eventcapacity=1,
             eventduration=1
-            )
+        )
         self.assertEqual(event.eventcreater, self.u1)
         self.assertEqual(event.eventcreateddate, datetime.now)
         self.assertEqual(event.eventname, "EventTest")
