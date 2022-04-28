cfd4f25e4620462b7c686ddc0b0a7dde76888658
Deniz Dikbıyık <denizdikbiyik@gmail.com>
2022-04-29 00:32:27 +0300

    Admin rights added

--- social/admin.py
+++ social/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Service, Event, ServiceApplication, EventApplication, Tag, UserRatings, UserComplaints, NotifyUser, Log, Communication, Like, Featured
 
 admin.site.register(UserProfile)
+admin.site.register(Service)
+admin.site.register(Event)
+admin.site.register(ServiceApplication)
+admin.site.register(EventApplication)
+admin.site.register(Tag)
+admin.site.register(UserRatings)
+admin.site.register(UserComplaints)
+admin.site.register(NotifyUser)
+admin.site.register(Log)
+admin.site.register(Communication)
+admin.site.register(Like)
+admin.site.register(Featured)
