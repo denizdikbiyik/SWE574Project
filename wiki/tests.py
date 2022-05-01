1121e383aed645a6ef67df2ac2e18a3a52d09340
Alper Turgal <alper.turgal@gmail.com>
2022-05-01 17:20:33 +0300

    [BUG] Wikidata search error when entered more than one word as keyword #200

--- wiki/tests.py
+++ wiki/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get(reverse('wiki'))
+        self.assertEqual(response.status_code, 200)
